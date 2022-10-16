58f01cf1889e46dee22279ba61e4b01ee8459694
d33ca62fe21c <mcollin4@citadel.edu>
2022-10-16 17:35:25 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+        name='pgbackup',
+        version='0.1.0',
+        author='Matthew Collins',
+        author_email='mcollin4@citadel.edu',
+        description='A utility for backing up PostgreSQL databases.',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://github.com/mcollinsece/pgbackup.git',
+        packages=find_packages('src')
+)
+
+
+
